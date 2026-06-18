3003b58ac9860db6724a2033c01be52f8b7816ac
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-06-18 17:22:36 +0200

    Update haos-hook.sh

--- buildroot-external/board/radxa/rock5b/haos-hook.sh
+++ buildroot-external/board/radxa/rock5b/haos-hook.sh
@@ -7,8 +7,12 @@ function haos_pre_image() {
     cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     cp "${BINARIES_DIR}/rockchip"/*.dtb "${BOOT_DATA}/"
 
-    mkdir -p "${BOOT_DATA}/overlays"
-    cp "${BINARIES_DIR}/rockchip"/*.dtbo "${BOOT_DATA}/overlays/"
+    if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
+        echo "Found .dtbo files in ${BINARIES_DIR}"
+        mkdir -p "${BOOT_DATA}/overlays"
+        cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
+    fi
+
     cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/haos-config.txt"
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 }
