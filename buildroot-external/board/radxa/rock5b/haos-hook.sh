1ec6566d4c9ac0cc360dfb4308796a222bce39e0
nyok92 <40342954+nyok92@users.noreply.github.com>
2026-06-22 02:36:18 +0200

    Update haos-hook.sh

--- buildroot-external/board/radxa/rock5b/haos-hook.sh
+++ buildroot-external/board/radxa/rock5b/haos-hook.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# shellcheck disable=SC2155
+set -x
 
 function haos_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
 
-    cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
-    cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/"
+    cp -t "${BOOT_DATA}" \
+        "${BINARIES_DIR}/boot.scr" \
+        "${BINARIES_DIR}/rockchip/"*.dtb
 
-    if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
-        echo "Found .dtbo files in ${BINARIES_DIR}"
-        mkdir -p "${BOOT_DATA}/overlays"
-        cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
-    fi
+    mkdir -p "${BOOT_DATA}/overlays"
+    cp "${BINARIES_DIR}/overlays/"*.dtbo "${BOOT_DATA}/overlays/"
 
-    cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/haos-config.txt"
-    cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
+    cp "${BOARD_DIR}/../boot-env-common.txt" "${BOOT_DATA}/haos-config.txt"
+    cat "${BOARD_DIR}/boot-env.txt" >> "${BOOT_DATA}/haos-config.txt"
+    cp "${BOARD_DIR}/../cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 }
 
 
